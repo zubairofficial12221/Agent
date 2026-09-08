f845cb3312455e72f568824dfe630828f3c7941b
Kaleel Ahamed Zubair <zubairofficial12221@gmail.com>
2026-09-08 10:00:27 +0530

    Implement home and html routes in app initialization
    Added home and html route functions to render index.html. Registered youtube_bp blueprint with a URL prefix.

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,18 @@
+import os , urlib.parse ,urllib.request , render_template
+from app.youtube_bp
+Gemini_api_key = "Gemini_API_Key ";
 
+def home():
+  return render_template("index.html");
+
+def create_app ():
+  app= Flask(_name_)
+  app.register_blueprint(youtube_bp, url_prefix="/youtube");
+@app.route("/html")
+
+
+def html():
+
+  return render_template("index.html");
+
+return app;
