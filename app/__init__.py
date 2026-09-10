5189bd8492a89ba7a4938e7454a30331c8e71b66
Kaleel Ahamed Zubair <zubairofficial12221@gmail.com>
2026-09-10 15:07:33 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
-from app.youtube import youtube_bp
+import os
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 
+from app.gmail import(
+    is_email_command,
+    extract_email,
+    create_gmail_url'
+    genereate_email_with_gemini
+)
 
 def create_app():
 
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(
         youtube_bp,
@@ -17,6 +25,12 @@ def create_app():
 
     @app.route("/html")
     def html():
-        return render_template("index.html")
+        return render_template("index.html")route
+
+    @app.route("/health")
+    def health():
+        return jsonify({
+            "status": "ok",
+        })
 
     return app
